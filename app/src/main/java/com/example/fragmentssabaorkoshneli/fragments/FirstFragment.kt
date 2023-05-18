142d619582c1e90831c833bb28cc3c74378c653b
Saba Orkoshneli <saba.orkoshneli16@gmail.com>
2023-05-18 22:58:06 +0400

    app

--- app/src/main/java/com/example/fragmentssabaorkoshneli/fragments/FirstFragment.kt
+++ app/src/main/java/com/example/fragmentssabaorkoshneli/fragments/FirstFragment.kt
@@ -1,7 +1,49 @@
 package com.example.fragmentssabaorkoshneli.fragments
 
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.example.fragmentssabaorkoshneli.R
+import com.example.fragmentssabaorkoshneli.R.id
 
 class FirstFragment: Fragment(R.layout.fragment_first) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // Find the "Ok" button using its ID
+        val okButton = view.findViewById<Button>(R.id.goToProfileFragment)
+        val okButton2 = view.findViewById<Button>(R.id.goToListFragment)
+
+        // Set a click listener on the "Ok" button
+        okButton.setOnClickListener {
+            // Navigate to the profile fragment
+            val firstName = view.findViewById<EditText>(R.id.firstFragmentFirstNameET).text.toString()
+            val lastName = view.findViewById<EditText>(R.id.firstFragmentLastNameET).text.toString()
+            val gpa = view.findViewById<EditText>(R.id.firstFragmentGPAET).text.toString().toDouble()
+
+            val bundle = Bundle().apply {
+                putString("name", firstName)
+                putString("last_name", lastName)
+                putDouble("gpa", gpa)
+            }
+
+            // Navigate to the profile fragment with arguments
+            findNavController().navigate(R.id.thirdFragment, bundle)
+        }
+
+        okButton.setOnLongClickListener {
+            view.findViewById<EditText>(R.id.firstFragmentFirstNameET).text.clear()
+            view.findViewById<EditText>(R.id.firstFragmentLastNameET).text.clear()
+            view.findViewById<EditText>(R.id.firstFragmentGPAET).text.clear()
+            true // Return true to consume the long-press event
+        }
+
+        okButton2.setOnClickListener{
+            findNavController().navigate(R.id.secondFragment)
+        }
+    }
+
 }
--- app/src/main/java/com/example/fragmentssabaorkoshneli/fragments/ThirdFragment.kt
+++ app/src/main/java/com/example/fragmentssabaorkoshneli/fragments/ThirdFragment.kt
@@ -1,7 +1,28 @@
 package com.example.fragmentssabaorkoshneli.fragments
 
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import com.example.fragmentssabaorkoshneli.R
 
 class ThirdFragment: Fragment(R.layout.fragment_third) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val name = arguments?.getString("name")
+        val lastName = arguments?.getString("last_name")
+        val gpa = arguments?.getDouble("gpa")
+
+        // Use the retrieved values as needed
+        // For example, update TextViews with the values
+        val nameTextView = view.findViewById<TextView>(R.id.ThirdFragmentFirstNameTV)
+        val lastNameTextView = view.findViewById<TextView>(R.id.ThirdFragmentLastNameTV)
+        val gpaTextView = view.findViewById<TextView>(R.id.ThirdFragmentGPATV)
+
+        nameTextView.text = name
+        lastNameTextView.text = lastName
+        gpaTextView.text = gpa.toString()
+    }
+
 }
